baa6140626fb05a5c500d1e6a4d743ebdd39102f
justtryhard <lexchernobrov@gmail.com>
2026-01-16 15:31:57 +0300

    FastAPI_task v2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import psycopg2
 from pydantic import BaseModel
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 
-class NotificationReceive(BaseModel):
+class NotificationReceive(BaseModel):  # модель данных для валидации
     user_id: str
     message: str
     send_at: datetime
@@ -44,4 +44,44 @@ def create_notification(notification: NotificationReceive):
         send_at=notification.send_at
     )
 
-    return {"status": "ok"}
+    return {"user_id": notification.user_id, "message": notification.message, "status": "scheduled"}
+
+
+@app.get("/notifications")
+def get_notifications_from_user(user_id: str = None):
+    if user_id:
+        scheduler.cursor.execute(
+            "SELECT id, user_id, message, send_at, status "
+            "FROM scheduled_notifications WHERE user_id = %s;",
+            (user_id,)
+        )
+    else:
+        scheduler.cursor.execute(
+                "SELECT id, user_id, message, send_at, status FROM scheduled_notifications"
+            )
+    lines = scheduler.cursor.fetchall()
+    list_of_lines = [
+        {"id": line[0],
+         "user_id": line[1],
+         "message": line[2],
+         "send_at": line[3],
+         "status": line[4]}
+        for line in lines]
+    return list_of_lines
+
+
+@app.delete("/notifications/{notification_id}")
+def remove_notification(notification_id: int):
+    scheduler.cursor.execute(
+        "DELETE FROM scheduled_notifications WHERE id = %s RETURNING id",
+        (notification_id,)
+    )
+    line = scheduler.cursor.fetchone()
+    scheduler.conn.commit()
+
+    if not line:
+        raise HTTPException(status_code=404, detail="Notification not found")
+
+    return {"id": notification_id, "status": "deleted"}
+
+
